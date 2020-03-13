acaebb72d0d0e063b0223746fae50bb98409fb1a
José María López <josemaria.lopez.palomo@toptal.com>
2020-03-13 09:04:46 +0100

    Brand new component created

---
+++ src/app/sku-form/sku-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sku-form',
+  templateUrl: './sku-form.component.html',
+  styleUrls: ['./sku-form.component.css']
+})
+export class SkuFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
